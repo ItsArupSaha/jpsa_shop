5274f6016009a4bfed6c65ec4aa5c82c55cc83a5
Grow With Arup <growwitharup@gmail.com>
2025-07-20 09:19:28 +0000

    i have no data in my database but still navigating to each page takes so

--- src/app/(main)/expenses/page.tsx
+++ src/app/(main)/expenses/page.tsx
@@ -1,5 +1,7 @@
 import ExpensesManagement from '@/components/expenses-management';
+import { getExpenses } from '@/lib/actions';
 
-export default function ExpensesPage() {
-  return <ExpensesManagement />;
+export default async function ExpensesPage() {
+  const initialExpenses = await getExpenses();
+  return <ExpensesManagement initialExpenses={initialExpenses} />;
 }
