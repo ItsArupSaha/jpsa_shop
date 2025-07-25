e72910718ef6b57f9e46095c2b8a9362e9f18b4e
Grow With Arup <growwitharup@gmail.com>
2025-07-25 13:29:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -16,6 +17,11 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://*.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev',
     ],
   },
 };
