57020e49c86f96f80e8811f33f16ad154784b609
Grow With Arup <growwitharup@gmail.com>
2025-07-30 10:40:48 +0000

    lets change things step by step okay? otherwise things will be overwhelm

--- src/components/book-management.tsx
+++ src/components/book-management.tsx
@@ -202,16 +202,38 @@ export default function BookManagement({ userId }: BookManagementProps) {
     const doc = new jsPDF();
     const dateString = format(closingStockDate, 'PPP');
     
-    doc.setFontSize(18);
-    doc.text(authUser.companyName || 'Bookstore', 14, 22);
-    doc.setFontSize(12);
-    doc.text('Closing Stock Report', 14, 30);
+    // Left side header
+    doc.setFontSize(16);
+    doc.setFont('helvetica', 'bold');
+    doc.text(authUser.companyName || 'Bookstore', 14, 20);
     doc.setFontSize(10);
+    doc.setFont('helvetica', 'normal');
+    doc.text(authUser.address || '', 14, 26);
+    doc.text(authUser.phone || '', 14, 32);
+
+    // Right side header
+    let yPos = 20;
+    if (authUser.bkashNumber) {
+        doc.text(`Bkash: ${authUser.bkashNumber}`, 200, yPos, { align: 'right' });
+        yPos += 6;
+    }
+    if (authUser.bankInfo) {
+        doc.text(`Bank: ${authUser.bankInfo}`, 200, yPos, { align: 'right' });
+    }
+
+    // Report Title
+    doc.setFontSize(14);
+    doc.setFont('helvetica', 'bold');
+    doc.text('Closing Stock Report', 105, 45, { align: 'center' });
+    doc.setFontSize(10);
+    doc.setFont('helvetica', 'normal');
     doc.setTextColor(100);
-    doc.text(`As of ${dateString}`, 14, 36);
+    doc.text(`As of ${dateString}`, 105, 51, { align: 'center' });
+    doc.setTextColor(0);
+
 
     autoTable(doc, {
-      startY: 45,
+      startY: 60,
       head: [['Title', 'Author', 'Stock']],
       body: closingStockData.map(book => [book.title, book.author, book.closingStock]),
     });
